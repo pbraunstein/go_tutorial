67ea17d19d3466e51b48a7b68c8f641cccca3f4e
pbraunstein <pbraunstein12@gmail.com>
2024-07-01 10:07:36 -0400

    adder implementation and more tests

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -2,14 +2,37 @@ package integers
 
 import (
     "testing"
-    "github.com/stretchr/testify/assert"
 )
 
 func TestAdder(t *testing.T) {
-    sum := Add(2, 2)
-    expected := 4
+    t.Run("Two positives", func(t *testing.T) {
+        sum := Add(2, 2)
+        expected := 4
 
-    assert.Equal(sum, expected)
+        AssertEqual(sum, expected, t)
+    })
+
+    t.Run("Two negatives", func(t *testing.T) {
+        sum := Add(-2, -2)
+        expected := -4
+
+        AssertEqual(sum, expected, t)
+    })
+
+    t.Run("one positive one negative", func(t *testing.T) {
+        sum := Add(-2, 2)
+        expected := 0
+
+        AssertEqual(sum, expected, t)
+    })
+}
+
+func AssertEqual(got, want int, t testing.TB) {
+    t.Helper()
+    
+    if got != want {
+        t.Errorf("got %d want %d", got, want)
+    }
 }
 
 
