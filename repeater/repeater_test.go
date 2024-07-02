bef5abd0b31d80412ee46d1038e82fa267758c5e
pbraunstein <pbraunstein12@gmail.com>
2024-07-02 09:08:17 -0400

    add benchmark tests

--- repeater/repeater_test.go
+++ repeater/repeater_test.go
@@ -27,3 +27,17 @@ func TestRepeater(t *testing.T) {
         assert.Equal(t, expected, repeated)
     })
 }
+
+func BenchmarkRepeater(b *testing.B) {
+    b.Run("O(10) repeats", func(b *testing.B) {
+        for i := 0; i < b.N; i++ {
+            Repeater("abc", 10)
+        }
+    })
+
+    b.Run("O(10000) repeats", func(b *testing.B) {
+        for i := 0; i < b.N; i++ {
+            Repeater("abc", 10000)
+        }
+    })
+}
