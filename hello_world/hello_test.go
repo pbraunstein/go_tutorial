faf90a98cfe503b14ca1af3f1e0af6805c728ff7
pbraunstein <pbraunstein12@gmail.com>
2024-07-02 08:48:57 -0400

    finish integration of assert library

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -1,55 +1,51 @@
 package main
 
-import "testing"
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
 
 func TestHello(t *testing.T) {
     t.Run("English saying hello to people", func(t *testing.T) {
         got := Hello("Philip", "english")
         want := "Hello Philip!"
 
-        assertCorrectMessage(t, got, want)
+        assert.Equal(t, want, got)
     })
 
     t.Run("English Hello with empty string", func(t *testing.T) {
         got := Hello("", "english")
         want := "Hello World!"
 
-        assertCorrectMessage(t, got, want)
+        assert.Equal(t, want, got)
     })
 
     t.Run("German saying hello to people", func(t *testing.T) {
         got := Hello("Philip", "german")
         want := "Hallo Philip!"
 
-        assertCorrectMessage(t, got, want)
+        assert.Equal(t, want, got)
     })
 
     t.Run("German Hello with empty string", func(t *testing.T) {
         got := Hello("", "german")
         want := "Hallo Welt!"
 
-        assertCorrectMessage(t, got, want)
+        assert.Equal(t, want, got)
     })
 
     t.Run("Greek saying hello to people", func(t *testing.T) {
         got := Hello("Philip", "greek")
         want := "Geia Philip!"
 
-        assertCorrectMessage(t, got, want)
+        assert.Equal(t, want, got)
     })
 
     t.Run("Greek Hello with empty string", func(t *testing.T) {
         got := Hello("", "greek")
         want := "Geia Kosmos!"
 
-        assertCorrectMessage(t, got, want)
+        assert.Equal(t, want, got)
     })
 }
 
-func assertCorrectMessage(t testing.TB, got, want string) {
-    t.Helper()
-
-    if got != want {
-        t.Errorf("got %q want %q", got, want)
-    }
-}
