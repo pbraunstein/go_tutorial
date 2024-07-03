0af18e61003e7da08d5cc7f9315c3a9fb9f471d5
pbraunstein <pbraunstein12@gmail.com>
2024-07-03 07:56:36 -0400

    producter implementation it compiles

---
+++ producter/producter.go
@@ -0,0 +1,22 @@
+package producter
+
+func product(toProduct []int) int {
+    result := 1
+
+    for _, v := range(toProduct) {
+        result *= v
+    }
+
+    return result
+}
+
+func Producter(numbersToProduct ...[]int) []int {
+    numResults := len(numbersToProduct)
+    products := make([]int, numResults)
+
+    for i, array := range(numbersToProduct) {
+        products[i] = product(array)
+    }
+
+    return products
+}
