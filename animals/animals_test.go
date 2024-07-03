a9cabb277c928f928bae7a73a0171c9a8136b526
pbraunstein <pbraunstein12@gmail.com>
2024-07-03 16:18:13 -0400

    Tests pass for animals

--- animals/animals_test.go
+++ animals/animals_test.go
@@ -2,13 +2,31 @@ package animals
 
 import (
     "testing"
-    "fmt"
     "github.com/stretchr/testify/assert"
 )
 
 func TestAnimals(t *testing.T) {
-    h := Human{}
-    fmt.Printf("%T", h)
-    assert.Equal(t, 1, 1)
+    animalsTests := []struct {
+        name string
+        animal Animal
+        greeting string
+        food string
+        hasThumbs bool
+    }{
+        {name: "Human", animal: Human{}, greeting: "Hello there", 
+        food: "Meatballs", hasThumbs: true},
+        {name: "Cat", animal: Cat{}, greeting: "Meow", 
+        food: "Milk", hasThumbs: false},
+        {name: "Dog", animal: Dog{}, greeting: "Woof", 
+        food: "Bone", hasThumbs: false},
+    }
+
+    for _, tt := range animalsTests {
+        t.Run(tt.name, func(t *testing.T) {
+            assert.Equal(t, tt.greeting, tt.animal.Speak())
+            assert.Equal(t, tt.food, tt.animal.Eat())
+            assert.Equal(t, tt.hasThumbs, tt.animal.HasThumbs())
+        })
+    }
 }
 
