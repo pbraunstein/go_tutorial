1635689edaebcbd0a9ad286925471a75d05be541
pbraunstein <pbraunstein12@gmail.com>
2024-07-05 13:05:18 -0400

    Shapes implementation

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,6 +1,32 @@
 package shapes
 
+import "math"
+
 type Shape interface {
     Area() float64
     Double()
+}
+
+type Square struct {
+    sideLength float64
+}
+
+func (s *Square) Area() float64 {
+    return s.sideLength * s.sideLength
+}
+
+func (s *Square) Double() {
+    s.sideLength *= 2
+}
+
+type Circle struct {
+    radius float64
+}
+
+func (c *Circle) Area() float64 {
+    return math.Pi * c.radius * c.radius
+}
+
+func (c *Circle) Double() {
+    c.radius *= 2
 }
