faacb2a6c07bc271413af9b62ac89bf3201e05f9
pbraunstein <pbraunstein12@gmail.com>
2024-07-06 21:34:55 -0400

    Use spy for sleep test

--- sleep/sleep_test.go
+++ sleep/sleep_test.go
@@ -3,14 +3,24 @@ package main
 import (
     "bytes"
     "testing"
+    "time"
     "github.com/stretchr/testify/assert"
 )
 
+type FakeSleeper struct {
+    calls int
+}
+
+func (f *FakeSleeper) Sleep(t time.Duration) {
+    f.calls += 1
+}
+
 func TestSleep(t *testing.T) {
     buffer := &bytes.Buffer{}
-    rs := RealSleeper{}
+    fs := FakeSleeper{}
 
-    Counter(buffer, &rs)
+    Counter(buffer, &fs)
 
     assert.Equal(t, "1\n2\n3\n4\n5\n", buffer.String())
+    assert.Equal(t, 4, fs.calls)
 }
