7ea45e7d34f3da050a2093c8ac8f47a47fdb6d70
pbraunstein <pbraunstein12@gmail.com>
2024-07-08 15:26:06 -0400

    speed up running in parallel using channels

--- fib/fib.go
+++ fib/fib.go
@@ -4,6 +4,11 @@ import "fmt"
 
 const MaxCalc = 50
 
+type Result struct {
+    key int
+    val int
+}
+
 func SlowFib(n int) int {
     if n == 0 {
         return 0
@@ -14,7 +19,23 @@ func SlowFib(n int) int {
     return SlowFib(n - 1) + SlowFib(n - 2)
 }
 
+func CollectFib() map[int]int {
+    toReturn := make(map[int]int)
+    channel := make(chan Result)
+    for i := 0; i <= MaxCalc; i++ {
+        go func() {
+            channel <- Result{i, SlowFib(i)}
+        }()
+    }
+    for i := 0; i <= MaxCalc; i++ {
+        r := <- channel
+        toReturn[r.key] = r.val
+    }
+    return toReturn
+}
+
 
 func main() {
-    fmt.Println(SlowFib(MaxCalc))
+    fibMap := CollectFib()
+    fmt.Println(fibMap)
 }
